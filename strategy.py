de15a9ea660b2b443d5ebd3a5a3d20c5b495221f
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2026-01-26 18:27:35 -0500

    Add ability to score

--- strategy.py
+++ strategy.py
@@ -56,7 +56,7 @@ class YahtzeeGame:
     #   Scoring
     #_______________________________________
 
-    # Score any choice from top section (1-6)
+    # Helper to score any choice from top section (1-6)
     def score_top(self, choice):
 
         if choice not in numeric_scores:
@@ -74,3 +74,73 @@ class YahtzeeGame:
         self.score_sheet[choice] = score
         return score
     
+    # Mark any choice on scoresheet
+    def score(self, choice):
+        if choice not in self.score_sheet:
+            raise ValueError(f"{choice} not valid option")
+        
+        if self.score_sheet[choice] is not None:
+            raise ValueError(f"{choice} already scored")
+        
+        if choice in numeric_scores:
+            return self.score_top(choice)
+        
+        if choice == "threekind":
+            if not any(self.dice.count(d) >= 3 for d in set(self.dice)):
+                self.score_sheet[choice] = 0
+                return 0
+            
+            all_dice = sum(self.dice)
+            
+            self.score_sheet[choice] = all_dice
+            return all_dice
+        
+        if choice == "fourkind":
+            if not any(self.dice.count(d) >= 4 for d in set(self.dice)):
+                self.score_sheet[choice] = 0
+                return 0
+            
+            all_dice = sum(self.dice)
+            
+            self.score_sheet[choice] = all_dice
+            return all_dice
+        
+        if choice == "fullhouse":
+            if sorted(self.dice.count(d) for d in set(self.dice)) != [2, 3] or len(set(self.dice)) != 1:
+                self.score_sheet[choice] = 0
+                return 0
+            
+            self.score_sheet[choice] = 25
+            return 25
+        
+        if choice == "smstraight":
+            if not any(seq.issubset(set(self.dice)) for seq in ({1,2,3,4}, {2,3,4,5}, {3,4,5,6})) or len(set(self.dice)) != 1:
+                self.score_sheet[choice] = 0
+                return 0
+            
+            self.score_sheet[choice] = 30
+            return 30
+        
+        if choice == "lgstraight":
+            if set(self.dice) not in ({1,2,3,4,5}, {2,3,4,5,6}) or len(set(self.dice)) != 1:
+                self.score_sheet[choice] = 0
+                return 0
+            
+            self.score_sheet[choice] = 40
+            return 40
+        
+        if choice == "yahtzee":
+            if len(set(self.dice)) != 1:
+                self.score_sheet[choice] = 0
+                return 0
+            
+            self.score_sheet[choice] = 50
+            return 50
+            
+        if choice == "chance":
+            all_dice = sum(self.dice)
+            
+            self.score_sheet[choice] = all_dice
+            return all_dice
+
+        raise ValueError(f"Unhandled category: {choice}")
