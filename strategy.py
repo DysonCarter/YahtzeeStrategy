2a8af9e69759c281af6b5282768fe7b31ee6f576
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2026-01-26 16:41:31 -0500

    Add YahtzeeGame class

--- strategy.py
+++ strategy.py
@@ -2,3 +2,27 @@ import random
 
 def roll_die():
     return random.randint(1, 6)
+
+# Solo game
+class YahtzeeGame:
+    def __init__(self):
+        self.dice = [0,0,0,0,0,0]
+
+        self.score_sheet = {
+            "aces":       0,
+            "twos":       0,
+            "threes":     0,
+            "fours":      0,
+            "fives":      0,
+            "sixes":      0,
+            "threekind":  0,
+            "fourkind":   0,
+            "fullhouse":  0,
+            "smstraight": 0,
+            "lgstraight": 0,
+            "yahtzee":    0,
+            "chance":     0,
+        }
+
+        # For Yahtzee Bonus
+        self.yahtzee_count = 0
