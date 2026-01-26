4917febda77d85bf482c6c3ddcfc309205291c65
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2026-01-26 16:51:53 -0500

    Add reroll

--- strategy.py
+++ strategy.py
@@ -4,6 +4,7 @@ import random
 class YahtzeeGame:
     def __init__(self):
         self.dice = [0,0,0,0,0]
+        self.reroll_count = 0
 
         self.score_sheet = {
             "aces":       None,
@@ -30,4 +31,11 @@ class YahtzeeGame:
     def roll_dice(self):
         for i in range(len(self.dice)):
             self.dice[i] = self.roll_die()
+
+    # Mask len 5 of which dice to reroll
+    # indices = [1,0,0,1,0] -> reroll die 0 and die 3
+    def reroll_dice(self, indices):
+        for i, reroll in enumerate(indices):
+            if reroll:
+                self.dice[i] = self.roll_die()
     
